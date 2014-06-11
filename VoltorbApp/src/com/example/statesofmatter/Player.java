0d1209ea8659da20ae2cf215c7a1b9f15ed116f4
jpriebe <jimmyorjimi@gmail.com>
2014-06-11 18:06:12 -0400

    Moh's stuff for Player

--- VoltorbApp/src/com/example/statesofmatter/Player.java
+++ VoltorbApp/src/com/example/statesofmatter/Player.java
@@ -1,12 +1,23 @@
 package com.example.statesofmatter;
 
-import java.util.ArrayList;
-
 public class Player {
 
     public String name;
-    public String password;
-
-    public ArrayList<Monster> team;
+    private String ID;//get from Android phone
+    private final int MAX_TEAM_SIZE = 6;
+    
+    public Monster[] team;
+    
+    public Player(String name, String ID){
+    	this.name = name;
+    	this.ID = ID;
+    	team = new Monster[MAX_TEAM_SIZE];//only 6 monsters allowed
+    }
+    
+    public void addMonster(Monster m, int position) throws Exception{
+    	if(position >= MAX_TEAM_SIZE) throw new Exception("Position is exceeds team size limit.  position = " + position);
+    	if(m == null) throw new Exception("Paramater Monster is null.");
+    	team[position] = m;
+    }
     
 }
